95e868ccc1b5c1da87b3294a647926b25575d970
SergiuszG <kruczy.borek@gmail.com>
2026-05-29 08:56:50 +0200

    Refactor RootLayout to use ReactNode type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
+import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "Sova House",
   description: "Domek w lesie w Kruczym Borku",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pl">
       <body>{children}</body>
