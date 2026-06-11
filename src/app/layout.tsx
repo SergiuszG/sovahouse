2e3eee905bd6393b9ed55a217f4e10288f4a5ca6
SergiuszG <kruczy.borek@gmail.com>
2026-06-11 06:12:33 +0200

    Integrate Google Analytics in RootLayout
    Added Google Analytics script to the RootLayout component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,35 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
+import Script from "next/script";
 
 export const metadata: Metadata = {
-title: "Sova House",
-description: "Domek w lesie w Kruczym Borku",
+  title: "Sova House",
+  description: "Domek w lesie w Kruczym Borku",
 };
 
 export default function RootLayout({
-children,
+  children,
 }: {
-children: ReactNode;
+  children: ReactNode;
 }) {
-return ( <html lang="pl"> <body>{children}</body> </html>
-);
+  return (
+    <html lang="pl">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-786Q1HDJPN"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-786Q1HDJPN');
+          `}
+        </Script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
